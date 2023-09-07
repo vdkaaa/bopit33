e8bd62367061d9a9d820e6788a15262ca3cf81d1
laboratorio <dsantander17@alumnos.utalca.cl>
2023-09-07 10:04:07 -0300

    Primer Commit

---
+++ app/src/main/java/app/dsantander17/bopit33/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.dsantander17.bopit33
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
