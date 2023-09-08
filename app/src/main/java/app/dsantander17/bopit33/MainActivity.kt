c59f01d62b9b5a746cafb8d9f37939f4ca7564ac
Diego Santander <dsantander17@alumnos.utalca.cl>
2023-09-07 23:14:10 -0300

    Tercer Commit

--- app/src/main/java/app/dsantander17/bopit33/MainActivity.kt
+++ app/src/main/java/app/dsantander17/bopit33/MainActivity.kt
@@ -1,11 +1,50 @@
 package app.dsantander17.bopit33
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import androidx.constraintlayout.motion.widget.MotionLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
+        val buttonOKAbout = findViewById<Button>(R.id.about_button)
+
+        // Button click listeners
+        buttonOKAbout.setOnClickListener {
+            val intentAbout = Intent(this, AboutActivity::class.java)
+            startActivity(intentAbout)
+        }
+
+
+
+
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                val intentPreferences = Intent(this, SettingsActivity::class.java)
+                startActivity(intentPreferences)
+                return true
+            }
+            R.id.action_about -> {
+                val intentPreferences = Intent(this, AboutActivity::class.java)
+                startActivity(intentPreferences)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
 }
