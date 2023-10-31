70af4ff46794a27c664a6ba4eef236d820fcfc29
dogod <dsantander17@alumnosutalca.cl>
2023-10-31 00:59:17 -0300

    Actualizacion Laboratorio 3 Acelerometro y Deteccion de evento Acelerometro

--- app/src/main/java/app/dsantander17/bopit33/PlayGame.kt
+++ app/src/main/java/app/dsantander17/bopit33/PlayGame.kt
@@ -13,6 +13,10 @@ import android.view.GestureDetector
 import android.view.MotionEvent
 import android.widget.Toast
 import androidx.preference.PreferenceManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 
 class PlayGame : AppCompatActivity() {
 
@@ -22,16 +26,76 @@ class PlayGame : AppCompatActivity() {
     private lateinit var mediaPlayerWin: MediaPlayer
     private lateinit var mediaPlayerLose: MediaPlayer
     private lateinit var gestureDetector: GestureDetector
+    private lateinit var sensorManager: SensorManager
+    private var accelerometer: Sensor? = null
+    private var sensorEventListener: SensorEventListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mediaPlayerBg = MediaPlayer.create(this, R.raw.background_music)
         mediaPlayerWin = MediaPlayer.create(this, R.raw.win)
         mediaPlayerLose = MediaPlayer.create(this, R.raw.lose)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         setContentView(R.layout.activity_play_game)
 
 
         gestureDetector = GestureDetector(this, MyGestureListener())
+
+        // Variables para almacenar los valores anteriores del acelerómetro
+        var previousX: Float = 0.0f
+        var previousY: Float = 0.0f
+        var previousZ: Float = 0.0f
+
+// Definición del umbral de cambio
+        val threshold = 1.5f // Puedes ajustar este valor según tu requerimiento
+
+
+        if(accelerometer==null)
+            finish();
+        //Configuracion del listener del sensor
+        sensorEventListener = object : SensorEventListener {
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                // Código para el cambio en la precisión del sensor
+            }
+
+            override fun onSensorChanged(event: SensorEvent) {
+                if (event.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+                    val xAxis = event.values[0]
+                    val yAxis = event.values[1]
+                    val zAxis = event.values[2]
+
+                    // Comparación con los valores anteriores
+                    if (hasSignificantChange(xAxis, yAxis, zAxis)) {
+                        // Hubo un cambio significativo en el acelerómetro
+                        println("Se detectó un evento en el acelerómetro")
+
+                        // Aquí podrías, por ejemplo, mostrar un mensaje, activar una función, etc.
+                         showAccelerometerEventMessage()
+                    }
+
+                    // Actualizar los valores anteriores con los valores actuales
+                    previousX = xAxis
+                    previousY = yAxis
+                    previousZ = zAxis
+                }
+
+            }
+
+            private fun hasSignificantChange(currentX: Float, currentY: Float, currentZ: Float): Boolean {
+                // Comparación con los valores anteriores y determinación de cambio significativo
+                return (Math.abs(currentX - previousX) > threshold ||
+                        Math.abs(currentY - previousY) > threshold ||
+                        Math.abs(currentZ - previousZ) > threshold)
+            }
+            private fun showAccelerometerEventMessage() {
+                val message = "¡Se detectó un cambio en el acelerómetro!"
+
+                // Muestra un Toast con el mensaje
+                Toast.makeText(this@PlayGame, message, Toast.LENGTH_SHORT).show()
+            }
+        }
+
         // Obtén una referencia al TextView en tu diseño de actividad
         val textViewPuntaje = findViewById<TextView>(R.id.textViewPuntaje) // Reemplaza R.id.textViewPuntaje con el ID de tu TextView
 
@@ -124,6 +188,8 @@ class PlayGame : AppCompatActivity() {
         if (mediaPlayerLose.isPlaying) {
             mediaPlayerLose.pause()
         }
+
+        sensorManager.unregisterListener(sensorEventListener)
     }
 
     override fun onDestroy() {
@@ -132,6 +198,13 @@ class PlayGame : AppCompatActivity() {
         mediaPlayerWin.release()
         mediaPlayerLose.release()
     }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         gestureDetector.onTouchEvent(event)
